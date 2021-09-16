b2dfe6b06e8d53bbf81fcb68faa248cf264a9266
Hendi Santika <hendisantika@gmail.com>
2021-09-16 09:31:53 +0700

    #8 - Add MvcConfig

---
+++ src/main/java/com/hendisantika/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 09.31
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+}
