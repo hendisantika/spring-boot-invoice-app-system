77e7518cc703cbc8b7a7ada6d6bb12cab523ed0c
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:07:56 +0700

    #54 - Add viewPhoto in ClientController

--- src/main/java/com/hendisantika/controller/ClientController.java
+++ src/main/java/com/hendisantika/controller/ClientController.java
@@ -6,8 +6,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +40,24 @@ public class ClientController {
 
     @Autowired
     private MessageSource messageSource;
+
+    /* ----- View Photo ----- */
+    // '.+' = returns the file name without format
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> viewPhoto(@PathVariable String filename) {
+
+        Resource resource = null;
+
+        try {
+            resource = uploadFileService.load(filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
 }
