566ec2693b16b484dae99e5649e65e58d2764e83
Hendi Santika <hendisantika@gmail.com>
2021-09-16 09:54:53 +0700

    #23 - Add UserRepository

---
+++ src/main/java/com/hendisantika/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
