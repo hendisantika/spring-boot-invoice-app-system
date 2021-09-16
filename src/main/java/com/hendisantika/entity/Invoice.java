20ec9283851282f2710017986b71c30bfcadf75b
Hendi Santika <hendisantika@gmail.com>
2021-09-16 09:41:43 +0700

    #15 - Add Invoice Entity

---
+++ src/main/java/com/hendisantika/entity/Invoice.java
@@ -0,0 +1,72 @@
+package com.hendisantika.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 09.39
+ */
+@Entity
+@Table(name = "invoices")
+@Data
+@AllArgsConstructor
+public class Invoice implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String description;
+
+    private String observation;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    private Client client;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "invoice_id")
+    private List<ItemInvoice> items;
+
+    public Invoice() {
+        this.items = new ArrayList<>();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
+
+    public Double getTotal() {
+        Double total = 0.0;
+        int size = items.size();
+
+        for (int i = 0; i < size; i++) {
+            total += items.get(i).calculateImport();
+        }
+
+        return total;
+    }
+
+}
