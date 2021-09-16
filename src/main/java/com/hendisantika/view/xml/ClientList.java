62863a7e83349b0dd4df5a82921b6bc1b880f9e9
Hendi Santika <hendisantika@gmail.com>
2021-09-16 10:18:48 +0700

    #43 - Add ClientList

---
+++ src/main/java/com/hendisantika/view/xml/ClientList.java
@@ -0,0 +1,29 @@
+package com.hendisantika.view.xml;
+
+import com.hendisantika.entity.Client;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 10.18
+ */
+@XmlRootElement(name = "clients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientList {
+
+    @XmlElement(name = "client")
+    public List<Client> clients;
+}
