bb1f3a1fd7ed089a1052e2677c01b1e8e25056fd
Hendi Santika <hendisantika@gmail.com>
2021-09-16 09:50:18 +0700

    #19 - Add Role Entity

--- src/main/java/com/hendisantika/entity/Role.java
+++ src/main/java/com/hendisantika/entity/Role.java
@@ -1,5 +1,16 @@
 package com.hendisantika.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import java.io.Serializable;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-invoice-app-system
@@ -9,5 +20,22 @@ package com.hendisantika.entity;
  * Date: 16/09/21
  * Time: 09.45
  */
-public class Role {
+@Entity
+@Table(name = "authorities", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "authority"})})
+@Data
+@AllArgsConstructor
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 2699282209351614842L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
 }
