3ff3673277127b16053fbdaf9c516f35fef12938
Hendi Santika <hendisantika@gmail.com>
2021-09-16 09:11:54 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootInvoiceAppSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInvoiceAppSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInvoiceAppSystemApplication.class, args);
+    }
+
+}
