cb72c80298a6fb84694433607272ed311aa27e0b
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:04:58 +0700

    #52 - Add bean in MainApp

--- src/main/java/com/hendisantika/SpringBootInvoiceAppSystemApplication.java
+++ src/main/java/com/hendisantika/SpringBootInvoiceAppSystemApplication.java
@@ -1,13 +1,37 @@
 package com.hendisantika;
 
+import com.hendisantika.service.UploadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootInvoiceAppSystemApplication {
+public class SpringBootInvoiceAppSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootInvoiceAppSystemApplication.class, args);
     }
 
+    @Autowired
+    UploadFileService uploadFileService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        uploadFileService.deleteAll();
+        uploadFileService.init();
+
+        // test passwords
+        String password = "12345";
+
+        for (int i = 0; i < 2; i++) {
+            String bcryptPassword = passwordEncoder.encode(password);
+            System.out.println(bcryptPassword);
+        }
+
+    }
 }
