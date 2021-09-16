96e40f1ecbcd6f51ac8ee3b142fa7a1c0a1abc3c
Hendi Santika <hendisantika@gmail.com>
2021-09-16 10:25:15 +0700

    #45 - Add ClientListXmlView

---
+++ src/main/java/com/hendisantika/view/xml/ClientListXmlView.java
@@ -0,0 +1,47 @@
+package com.hendisantika.view.xml;
+
+import com.hendisantika.entity.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.xml.MarshallingView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 10.18
+ */
+@Component("list.xml")
+public class ClientListXmlView extends MarshallingView {
+
+    @Autowired
+    public ClientListXmlView(Jaxb2Marshaller marshaller) {
+        super(marshaller);
+    }
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+                                           HttpServletResponse response) throws Exception {
+
+        model.remove("title");
+        model.remove("page");
+
+        @SuppressWarnings("unchecked")
+        Page<Client> clients = (Page<Client>) model.get("clients");
+
+        model.remove("clients");
+
+        model.put("clientList", new ClientList(clients.getContent()));
+
+        super.renderMergedOutputModel(model, request, response);
+    }
+}
