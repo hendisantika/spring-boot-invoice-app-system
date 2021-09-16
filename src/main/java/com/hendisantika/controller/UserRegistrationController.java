fe96db43e142f1b97e2c8629696886d2a7977f42
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:26:02 +0700

    #72 - Add showRegistrationForm in UserRegistrationController

--- src/main/java/com/hendisantika/controller/UserRegistrationController.java
+++ src/main/java/com/hendisantika/controller/UserRegistrationController.java
@@ -1,10 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,11 @@ public class UserRegistrationController {
 
     @Autowired
     private MessageSource messageSource;
+
+    @GetMapping("/signup")
+    public String showRegistrationForm(Model model, Locale locale) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
 }
