522be8e41b28c8aecb5cb096cb71f558c837e4f9
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:03:32 +0700

    #51 - Add configuredGlobal in SpringSecurityConfig

--- src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.config;
 import com.hendisantika.service.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -44,4 +45,43 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe();
 
     }
+
+    @Autowired
+    public void configuredGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        // JPA
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+
+
+//		 // JDBC
+//		 // Spring security jdbc auth builder.jdbcAuthentication()
+//
+//		 .dataSource(dataSource) .passwordEncoder(passwordEncoder)
+//		 .usersByUsernameQuery("select username, password, enabled from users where username=?"
+//		 )
+//		 .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a
+//		 .user_id=u.id) where u.username=?"
+//		 );
+//
+//
+//
+//		 PasswordEncoder encoder = this.passwordEncoder;
+//
+//		 // UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//		 // Manera 1 // UserBuilder users = User.builder().passwordEncoder(password ->
+//		 encoder.encode(password));
+//
+//		 // Manera 2 // UserBuilder users =
+//		 User.builder().passwordEncoder(encoder::encode);
+//
+//		 // Manera 3 UserBuilder users = User.builder().passwordEncoder(password -> {
+//		 return encoder.encode(password); });
+//
+//		 builder.inMemoryAuthentication()
+//		 .withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+//		 .withUser(users.username("test").password("12345").roles("USER"));
+
+
+    }
+
 }
