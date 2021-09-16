76ca7b0170915ce4fc9b775b4b9dbe73cca81975
Hendi Santika <hendisantika@gmail.com>
2021-09-16 10:25:04 +0700

    #44 - Add ClientCsvView

---
+++ src/main/java/com/hendisantika/view/csv/ClientCsvView.java
@@ -0,0 +1,58 @@
+package com.hendisantika.view.csv;
+
+import com.hendisantika.entity.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 10.20
+ */
+@Component("list.csv")
+public class ClientCsvView extends AbstractView {
+
+    public ClientCsvView() {
+        setContentType("text/csv");
+    }
+
+    protected boolean generatesDownloadContent() {
+        return true;
+    }
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+                                           HttpServletResponse response) throws Exception {
+
+        response.setHeader("Content-Disposition", "attachment; filename=\"client.csv\"");
+        response.setContentType(getContentType());
+
+        @SuppressWarnings("unchecked")
+        Page<Client> clients = (Page<Client>) model.get("clients");
+
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+        String[] header = {"id", "firstName", "lastName", "email", "createAt"};
+        beanWriter.writeHeader(header);
+
+        for (Client client : clients) {
+            beanWriter.write(client, header);
+        }
+
+        beanWriter.close();
+
+
+    }
+}
