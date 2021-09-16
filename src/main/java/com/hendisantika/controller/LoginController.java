a460ada6ce535e8e00d9e7b8aa3b458a7b911b9b
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:24:45 +0700

    #69 - Add LoginController

---
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/09/21
+ * Time: 11.24
+ */
+@Controller
+public class LoginController {
+
+    @Autowired
+    private MessageSource messageSource;
+}
