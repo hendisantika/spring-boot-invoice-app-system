ecc4f2c12597c426bf24222369ca4a6891c4c0e8
Hendi Santika <hendisantika@gmail.com>
2021-09-16 11:19:42 +0700

    #63 - Add view in InvoiceController

--- src/main/java/com/hendisantika/controller/InvoiceController.java
+++ src/main/java/com/hendisantika/controller/InvoiceController.java
@@ -1,13 +1,20 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Invoice;
 import com.hendisantika.service.ClientService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +38,22 @@ public class InvoiceController {
     @Autowired
     private MessageSource messageSource;
 
+    /* ----- View Invoice[id] ----- */
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash, Locale locale) {
+
+        Invoice invoice = clientService.fetchInvoiceByIdWithClientWithInvoiceItemWithProduct(id);
+
+        if (invoice == null) {
+            flash.addAttribute("error", messageSource.getMessage("text.invoice.flash.db.error", null, locale));
+            return "redirect:/list";
+        }
+
+        model.addAttribute("invoice", invoice);
+        model.addAttribute("title", String.format(messageSource.getMessage("text.invoice.view.title", null, locale),
+                invoice.getDescription()));
+
+        return "invoice/view";
+    }
 
 }
